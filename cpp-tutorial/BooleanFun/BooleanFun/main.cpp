5a6532a1db63b5be53d09c7c36d56bbc270ae435
JT <jarred.ro@outlook.com>
2022-03-03 14:50:29 -0500

    adding first tutorial files

---
+++ cpp-tutorial/BooleanFun/BooleanFun/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	bool isRaining = false;
+
+	cout << boolalpha << isRaining << endl;
+
+	return 0;
+}
---
+++ cpp-tutorial/TextFun/TextFun/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>	
+#include <string>
+
+using namespace std;
+
+int main() {
+
+	char singleChar = 'a';
+	string myName = "JT";
+	cout << singleChar << endl;
+	cout << "Hello " << myName;
+
+
+	return 0;
+}
